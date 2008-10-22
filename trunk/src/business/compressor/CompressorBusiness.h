e703f54f62a24e416a22bf6e1143566098fb4cca
flororsi <flororsi@5b38038c-6156-0410-9d9c-ef0dca45031e>
2008-10-22 20:09:02 +0000

    Compresion listo

---
+++ trunk/src/business/compressor/CompressorBusiness.h
@@ -0,0 +1,40 @@
+#ifndef COMPRESSORBUSINESS_H_
+#define COMPRESSORBUSINESS_H_
+
+
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <map>
+
+#define BITS 12                   /* Cantidad de bits para la tabla*/
+#define SHIFT (BITS-8)    
+#define MAX_VALUE (1 << BITS) - 1 
+#define MAX_CODE MAX_VALUE - 1    
+#define TABLA 5021
+
+
+using namespace std;
+class CompressorBusiness {
+	
+private:
+	
+	map<int,int> map_code;
+	map<int,unsigned int> map_prefix;
+	map<int,unsigned char> map_append;
+	int find_code(int prefijo,unsigned int character);
+	void put_code(FILE *output,unsigned int code);
+	unsigned int get_code(FILE *input);
+	
+	
+public:
+	CompressorBusiness();
+	virtual ~CompressorBusiness();
+	int compress(FILE * input,FILE * output);
+	int decompress(FILE * input,FILE * output);
+};
+
+
+
+#endif /*COMPRESSORBUSINESS_H_*/
+
